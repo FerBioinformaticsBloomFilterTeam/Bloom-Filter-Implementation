134ba0fbb877bcde3d640195cd5ff5eb12ca17a3
edi-smoljan <edi.smoljan@fer.hr>
2014-12-23 14:16:20 +0100

    Poboljšan bloom filter
    Ispravljen bug s ifdefom, dodana mogućnost ispisa bloom filtera (hexa),
    dodan novi inicijalizator koji prima samo n i p te izračunava optimalne
    k i m.

--- EdiBloomFilterC/GenomeBloomFilterGCC/bloom_filter.h
+++ EdiBloomFilterC/GenomeBloomFilterGCC/bloom_filter.h
@@ -1,6 +1,7 @@
 #include <inttypes.h>
 
-void init(float p, uint32_t n, uint16_t k);
+void init_opt(float p, uint32_t n);
+void init(float p, uint32_t n, uint8_t k);
 int test(void* obj, unsigned int len);
 void add(void* obj, unsigned int len);
 void clear(void);
