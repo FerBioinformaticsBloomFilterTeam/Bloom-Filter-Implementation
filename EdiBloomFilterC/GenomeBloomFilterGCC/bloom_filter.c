4d7dd3dea19ecf685d35406b9525fee70267364b
edi-smoljan <edi.smoljan@fer.hr>
2014-12-23 12:08:45 +0100

    ispravak bugova bloom filtera
    kinda popravljeni bugovi, malo izmijenjen način rada (ne stvara se svaki
    put polje za indekse), ubačen alternativni način izračuna m (fbit2)

--- EdiBloomFilterC/GenomeBloomFilterGCC/bloom_filter.c
+++ EdiBloomFilterC/GenomeBloomFilterGCC/bloom_filter.c
@@ -7,74 +7,81 @@ uint8_t *filter = 0;
 uint32_t filter_bit_len = 0;
 uint32_t filter_byte_len = 0;
 uint32_t (*hashes[2])(void*, unsigned int, uint32_t);
-uint16_t k = 0;
+uint8_t k = 0;
+uint32_t *indices = 0;
 
-void init(float p, uint32_t n, uint16_t k1) {
+void init(float p, uint32_t n, uint8_t k1) {
+    float fbit = -logf(p) / powf(logf(2), 2.0f) * n;
+    float fbit2 = 1.0f / (1.0f - powf(1.0f - powf(p, 1.0f / k1), 1.0f / k1 / n));
     hashes[0] = fnv_hash;
     hashes[1] = murmur_hash;
     k = k1;
-    filter_bit_len = (uint32_t) ceilf(- n * logf(p) / (powf(logf(2), 2.0f)));
-    printf("Bit len: %u", filter_bit_len);
+    filter_bit_len = ceilf(fbit);
+    printf("Bit len: %u\n", filter_bit_len);
     filter_byte_len = filter_bit_len / 8 + (filter_bit_len % 8 != 0 ? 1 : 0);
     filter = (uint8_t *) calloc(filter_byte_len, 1);
+    indices = (uint32_t *) calloc(k, sizeof(uint32_t));
 }
 
-uint32_t* get_indices(void* obj, unsigned int len) {
-    uint32_t *indices = (uint32_t *) calloc(k, sizeof(uint32_t));
+void refresh_indices(void* obj, unsigned int len) {
     uint32_t i = 0;
     uint32_t fst_hash = hashes[0](obj, len, 2166136261u);
     uint32_t snd_hash = hashes[1](obj, len, 2166136261u);
     for(i = 0; i < k; i++) {
         indices[i] = (fst_hash + i * snd_hash) % filter_bit_len;
     }
-    return indices;
 }
 
 int get_bit(uint32_t index) {
     uint32_t byte_ind, bit_ind;
+    #ifdef DEBUG
     if (index < 0 || index >= filter_bit_len)
         return -1;
+    #endif // DEBUG
     byte_ind = index >> 3;
     bit_ind = index & 7;
     return (filter[byte_ind] >> bit_ind) & 1u;
 }
 
 int test(void* obj, unsigned int len) {
-    uint32_t *indices = get_indices(obj, len);
     uint16_t i;
     int res = 1;
+    refresh_indices(obj, len);
     for (i = 0; i < k; i++) {
         int bit = get_bit(indices[i]);
         if (bit == 0) {
             res = 0;
             break;
-        } else if (bit == -1) {
+        }
+        #ifdef DEBUG
+        else if (bit == -1) {
             printf("error in get_bit");
             exit(2);
         }
+        #endif
     }
-    free(indices);
     return res;
 }
 
 void set_bit(uint32_t index) {
     uint32_t byte_ind, bit_ind;
+    #ifdef DEBUG
     if (index < 0 || index >= filter_bit_len) {
         printf("error in set_bit");
         exit(2);
     }
+    #endif
     byte_ind = index >> 3;
     bit_ind = index & 7;
     filter[byte_ind] |= 1u << bit_ind;
 }
 
 void add(void* obj, unsigned int len) {
-    uint32_t *indices = get_indices(obj, len);
     uint16_t i;
+    refresh_indices(obj, len);
     for (i = 0; i < k; i++) {
         set_bit(indices[i]);
     }
-    free(indices);
 }
 
 void clear(void) {
@@ -82,4 +89,5 @@ void clear(void) {
     filter_bit_len = 0;
     filter_byte_len = 0;
     k = 0;
+    free(indices);
 }
--- EdiBloomFilterC/GenomeBloomFilterGCC/main.c
+++ EdiBloomFilterC/GenomeBloomFilterGCC/main.c
@@ -10,6 +10,25 @@ int main()
     uint32_t len = strlen(cont);
     uint32_t seed = 2166136261u;
     uint32_t res = murmur_hash(cont, len, seed);
+    uint32_t i = 0;
+    uint32_t n = 23;
     printf("Seed: %u\nSol: %u\nSol 0x: %x\n", seed, res, res);
+    init(0.01, n, 2);
+    while (i < n) {
+        char a[30];
+        int res;
+        scanf("%s", a);
+        res = test(a, strlen(a));
+        if (res == 0) {
+            printf("Not in set\n");
+        } else if (res == 1) {
+            printf("Probably in set\n");
+        } else {
+            printf("error\n");
+        }
+        add(a, strlen(a));
+        i++;
+    }
+    clear();
     return 0;
 }
