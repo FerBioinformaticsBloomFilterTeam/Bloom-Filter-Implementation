3e5804f2952db389a1f2042b4de273807448f80d
Filip <filip@ubuntu.(none)>
2015-01-11 23:40:35 +0100

    removed some bugs

--- FajdoBloomFilterJava/BloomFilterImplementation/src/hr/bio/project/bloomfilter/BloomFilter.java
+++ FajdoBloomFilterJava/BloomFilterImplementation/src/hr/bio/project/bloomfilter/BloomFilter.java
@@ -20,8 +20,8 @@ public class BloomFilter {
 		hasher = new Hasher();
 		k = (int) Math.ceil(-Math.log(p));
 		hashes = new int[k];
-		System.out.println(m);
-		System.out.println(k);
+		//System.out.println(m);
+		//System.out.println(k);
 	}
 	
 	public void addElemToBloom(String fastaPart) {
@@ -63,13 +63,21 @@ public class BloomFilter {
 	private void addBloom(char c, int index) {
 		switch(c) {
 			case 0: filter[index] = (char) (filter[index] | 0b10000000);
+					break;
 			case 1: filter[index] = (char) (filter[index] | 0b01000000);
+					break;
 			case 2: filter[index] = (char) (filter[index] | 0b00100000);
+					break;
 			case 3: filter[index] = (char) (filter[index] | 0b00010000);
+					break;
 			case 4: filter[index] = (char) (filter[index] | 0b00001000);
+					break;
 			case 5: filter[index] = (char) (filter[index] | 0b00000100);
+					break;
 			case 6: filter[index] = (char) (filter[index] | 0b00000010);
+					break;
 			case 7: filter[index] = (char) (filter[index] | 0b00000001);
+					break;
 		}
 	}
 	
@@ -77,13 +85,21 @@ public class BloomFilter {
 		int tester = 0;
 		switch(c) {
 			case 0: tester = ((char) (filter[index] & 0b10000000))>>7;
+					break;
 			case 1: tester = ((char) (filter[index] & 0b01000000))>>6;
+					break;
 			case 2: tester = ((char) (filter[index] & 0b00100000))>>5;
+					break;
 			case 3: tester = ((char) (filter[index] & 0b00010000))>>4;
+					break;
 			case 4: tester = ((char) (filter[index] & 0b00001000))>>3;
+					break;
 			case 5: tester = ((char) (filter[index] & 0b00000100))>>2;
+					break;
 			case 6: tester = ((char) (filter[index] & 0b00000010))>>1;
+					break;
 			case 7: tester = (char) (filter[index] & 0b00000001);
+					break;
 		}
 		return tester;
 	}
--- FajdoBloomFilterJava/BloomFilterImplementation/src/hr/bio/project/bloomtest/TestBloomFilter.java
+++ FajdoBloomFilterJava/BloomFilterImplementation/src/hr/bio/project/bloomtest/TestBloomFilter.java
@@ -62,13 +62,21 @@ public class TestBloomFilter {
 	private void addBloom(char c, int index) {
 		switch(c) {
 			case 0: filter[index] = (char) (filter[index] | 0b10000000);
+					break;
 			case 1: filter[index] = (char) (filter[index] | 0b01000000);
+					break;
 			case 2: filter[index] = (char) (filter[index] | 0b00100000);
+					break;
 			case 3: filter[index] = (char) (filter[index] | 0b00010000);
+					break;
 			case 4: filter[index] = (char) (filter[index] | 0b00001000);
+					break;
 			case 5: filter[index] = (char) (filter[index] | 0b00000100);
+					break;
 			case 6: filter[index] = (char) (filter[index] | 0b00000010);
+					break;
 			case 7: filter[index] = (char) (filter[index] | 0b00000001);
+					break;
 		}
 	}
 	
@@ -76,13 +84,21 @@ public class TestBloomFilter {
 		int tester = 0;
 		switch(c) {
 			case 0: tester = ((char) (filter[index] & 0b10000000))>>7;
+					break;
 			case 1: tester = ((char) (filter[index] & 0b01000000))>>6;
+					break;
 			case 2: tester = ((char) (filter[index] & 0b00100000))>>5;
+					break;
 			case 3: tester = ((char) (filter[index] & 0b00010000))>>4;
+					break;
 			case 4: tester = ((char) (filter[index] & 0b00001000))>>3;
+					break;
 			case 5: tester = ((char) (filter[index] & 0b00000100))>>2;
+					break;
 			case 6: tester = ((char) (filter[index] & 0b00000010))>>1;
+					break;
 			case 7: tester = (char) (filter[index] & 0b00000001);
+					break;
 		}
 		return tester;
 	}
