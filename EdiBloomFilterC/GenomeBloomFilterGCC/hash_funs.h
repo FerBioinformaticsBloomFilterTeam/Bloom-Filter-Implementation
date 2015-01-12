da2100e81e9c9ef9bc83a3508d2683584e701b81
edi-smoljan <edi.smoljan@fer.hr>
2015-01-13 00:14:10 +0100

    A few minor improvements made
    Commented the bloom_filter and hash_funs modules, removed pos results
    from test_main

--- EdiBloomFilterC/GenomeBloomFilterGCC/hash_funs.h
+++ EdiBloomFilterC/GenomeBloomFilterGCC/hash_funs.h
@@ -1,7 +1,9 @@
 #include <inttypes.h>
 
 //seed for fnv = 2166136261u
+/*Calculates the 32-bit version of the FNV hash.*/
 uint32_t fnv_hash(void* bytes, unsigned int len, uint32_t seed);
+/*Calculates the 32-bit version of the murmur hash.*/
 uint32_t murmur_hash(void* bytes, unsigned int len, uint32_t seed);
 
 void fnv_set_prime(uint32_t prime);
